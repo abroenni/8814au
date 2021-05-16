d95d1249d0c0ae2dfb50cf0c1fd88160161bed73
abroenni <abroenni@wolke7.net>
2021-05-16 15:09:31 +0800

    corrected build warning

--- hal/hal_intf.c
+++ hal/hal_intf.c
@@ -284,8 +284,9 @@ u8 rtw_hal_rfpath_init(_adapter *adapter)
 
 #ifdef CONFIG_RTL8814A
 if (IS_HARDWARE_TYPE_8814A(adapter)) {
-	RTW_INFO("Hardware Adapter: 8814AU\n");
 	enum bb_path tx_bmp, rx_bmp;
+
+	RTW_INFO("Hardware Adapter: 8814AU\n");
 	hal_data->rf_type = rtl8814a_rfpath_decision(adapter);
 	rf_type_to_default_trx_bmp(hal_data->rf_type, &tx_bmp, &rx_bmp);
 	hal_data->trx_path_bmp = (tx_bmp << 4) | rx_bmp;
