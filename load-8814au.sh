7cbc6a43d219c3ca268ee5b398637b2fee486a93
abroenni <abroenni@wolke7.net>
2021-05-24 16:22:40 +0800

    added module loading script; mainly for quicker debugging

---
+++ load-8814au.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+insmod 8814au.ko \
+    rtw_led_ctrl=0 \
+    rtw_country_code=AU \
+    rtw_vht_enable=2 \
+    rtw_switch_usb_mode=0 \
+    rtw_80211d=1 \
+    rtw_tx_nss=3 \
+    rtw_tx_nss=3 \
+    rtw_rx_stbc=3 \
+    # rtw_power_mgnt=0 \
